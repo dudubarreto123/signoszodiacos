5691b2d9400ee251f2167c360c7a2ca844acb052
Eduardo Paiva Barreto <120474364+dudubarreto123@users.noreply.github.com>
2023-06-28 08:37:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function signos() 
+    const elementoSignos= document.querySelector(".signos");
+ 
+    var img = document.querySelector('#imagem');
+
+    const frase = document.querySelector('.frase')
+
+signos = new Array('Áries', 'Touro', 'Gêmeos', 'Câncer', 'Leão', 'Virgem', 'Libra', 'Escorpião', 'Sagitário', 'Capricórnio', 'Aquario', 'Peixes');
